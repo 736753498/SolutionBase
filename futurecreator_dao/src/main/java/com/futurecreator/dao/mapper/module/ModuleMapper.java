e873d5f4c3146b2026852a0089afc5f460d5dc46
Zhang <3484868205@qq.com>
2023-07-02 18:45:03 +0800

    ModuleMapper finished

--- futurecreator_dao/src/main/java/com/futurecreator/dao/mapper/module/ModuleMapper.java
+++ futurecreator_dao/src/main/java/com/futurecreator/dao/mapper/module/ModuleMapper.java
@@ -1,7 +1,15 @@
 package com.futurecreator.dao.mapper.module;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.futurecreator.dao.pojo.module.Module;
+import com.futurecreator.dao.pojo.module.R.ModuleR;
 
 public interface ModuleMapper extends BaseMapper<Module> {
+    /**
+     * 得到所有模块信息
+     * @return
+     */
+    List<ModuleR> getAllModuleR();
 }
