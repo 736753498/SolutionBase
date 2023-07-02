74bf07db588caf7533d5b8f293cba830fd4295e8
Zhang <3484868205@qq.com>
2023-07-02 15:15:35 +0800

    reset all pojo increment type

--- futurecreator_dao/src/main/java/com/futurecreator/dao/pojo/module/RelModuleInterface.java
+++ futurecreator_dao/src/main/java/com/futurecreator/dao/pojo/module/RelModuleInterface.java
@@ -1,5 +1,7 @@
 package com.futurecreator.dao.pojo.module;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RelModuleInterface {
+    @TableId(type = IdType.AUTO)
     private Long id;
     private Long moduleId;
     private Long interfaceId;
