d0d53f2fe9e61100b8a6b03ede2e3241c9fa07c0
Zhang <3484868205@qq.com>
2023-07-02 12:36:38 +0800

    add MybatisPlusConfig and WebMVCConfig

---
+++ searchquestion/src/main/java/com/futurecreator/searchquestion/config/WebMVCConfig.java
@@ -0,0 +1,9 @@
+package com.futurecreator.searchquestion.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+}
