2c64437cf5e66f42f31593e9ce5743842694f290
Zhang <3484868205@qq.com>
2023-07-02 15:17:26 +0800

    add functions to UserMapper

--- futurecreator_dao/src/main/java/com/futurecreator/dao/mapper/user/UserMapper.java
+++ futurecreator_dao/src/main/java/com/futurecreator/dao/mapper/user/UserMapper.java
@@ -1,5 +1,27 @@
 package com.futurecreator.dao.mapper.user;
+import org.apache.ibatis.annotations.Param;
+import com.futurecreator.dao.pojo.user.User;
 
 public interface UserMapper{
+    /**
+     * 判断是否有phone,password对应的用户
+     * @param phone
+     * @param password
+     * @return
+     */
+    Boolean isUserValid(@Param("phone") String phone, @Param("password") String password);
 
+    /**
+     * 插入对应的user
+     * @param user
+     * @return
+     */
+    int addUser(User user);
+
+    /**
+     * 根据id更新user
+     * @param user
+     * @return
+     */
+    int updateSelective(User user);
 }
