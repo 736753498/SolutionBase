ae8c33cf78e5462d1cfc1bfaa10c3a0ef2f53264
Zhang <3484868205@qq.com>
2023-07-02 10:39:55 +0800

    init father project(futureCreator) and son project(searchQuestion) and read

---
+++ searchquestion/src/main/java/com/futurecreator/searchquestion/SearchquestionApplication.java
@@ -0,0 +1,13 @@
+package com.futurecreator.searchquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchquestionApplication.class, args);
+	}
+
+}
---
+++ searchquestion/src/test/java/com/futurecreator/searchquestion/SearchquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurecreator.searchquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
