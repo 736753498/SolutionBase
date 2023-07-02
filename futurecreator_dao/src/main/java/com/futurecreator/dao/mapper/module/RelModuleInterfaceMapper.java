40f0ab6013d567f9fdb8a09ff6dab25228433b30
Zhang <3484868205@qq.com>
2023-07-02 17:29:50 +0800

    remove rel pojo

--- futurecreator_dao/src/main/java/com/futurecreator/dao/mapper/module/RelModuleInterfaceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.futurecreator.dao.mapper.module;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.futurecreator.dao.pojo.module.RelModuleInterface;
-
-public interface RelModuleInterfaceMapper extends BaseMapper<RelModuleInterface> {
-}
