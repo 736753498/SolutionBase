1c6f69c61bfe5f74179e24c5593ac713ffd9f323
Zhang <3484868205@qq.com>
2023-07-03 16:23:22 +0800

    ex handler add annotation

--- searchquestion/src/main/java/com/futurecreator/searchquestion/handler/AllExceptionHandler.java
+++ searchquestion/src/main/java/com/futurecreator/searchquestion/handler/AllExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.futurecreator.searchquestion.handler;
 
-import com.futurecreator.common.enums.BusinessCodeEnum;
+import com.futurecreator.common.enums.TransCode;
 import com.futurecreator.common.vo.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //异常处理
 @ControllerAdvice
 public class AllExceptionHandler {
+    /**
+     * 当业务出现异常时给client返回sys error code
+     * @param ex
+     * @return
+     */
     @ExceptionHandler
     @ResponseBody
     public Result doException(Exception ex){
         ex.printStackTrace();
-        return Result.fail(BusinessCodeEnum.DEFAULT_SYS_ERROR);
+        return Result.fail(TransCode.DEFAULT_SYS_ERROR);
     }
 }
