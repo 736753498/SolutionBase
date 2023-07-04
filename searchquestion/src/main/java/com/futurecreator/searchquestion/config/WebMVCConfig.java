5bcdef486c925df03dfc281b76a3664088806f5c
Zhang <3484868205@qq.com>
2023-07-04 15:31:06 +0800

    WebMVCConfig and interceptor of login

--- searchquestion/src/main/java/com/futurecreator/searchquestion/config/WebMVCConfig.java
+++ searchquestion/src/main/java/com/futurecreator/searchquestion/config/WebMVCConfig.java
@@ -1,9 +1,28 @@
 package com.futurecreator.searchquestion.config;
 
+import com.futurecreator.searchquestion.handler.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
 
+    //跨域映射
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("http://zhangjiaweii.top/");
+    }
+
+    //需要登陆的各个路径
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("auth/logout")
+                .addPathPatterns("/info/**");
+    }
 }
