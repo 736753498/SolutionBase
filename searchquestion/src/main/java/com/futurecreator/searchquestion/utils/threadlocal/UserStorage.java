879e5aa34f0019c113fd75618c5ac0b9075aa03e
Zhang <3484868205@qq.com>
2023-07-04 15:30:44 +0800

    user storage add property of token, use of param init

--- searchquestion/src/main/java/com/futurecreator/searchquestion/utils/threadlocal/UserStorage.java
+++ searchquestion/src/main/java/com/futurecreator/searchquestion/utils/threadlocal/UserStorage.java
@@ -7,20 +7,31 @@ import com.futurecreator.dao.pojo.user.User;
  */
 public class UserStorage {
     private static ThreadLocal<User> userThreadLocal;
+    private static ThreadLocal<String> userTokenThreadLocal;
 
     static {
         userThreadLocal=new ThreadLocal<>();
+        userTokenThreadLocal=new ThreadLocal<>();
     }
 
     public static void setUser(User user){
         userThreadLocal.set(user);
     }
 
+    public static void setToken(String token){
+        userTokenThreadLocal.set(token);
+    }
+
     public static User getUser(){
         return userThreadLocal.get();
     }
 
+    public static String getToken(){
+        return userTokenThreadLocal.get();
+    }
+
     public static void removeUser(){
         userThreadLocal.remove();
+        userTokenThreadLocal.remove();
     }
 }
